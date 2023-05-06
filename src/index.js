ddd1181b58c05848fa11b3e5140e65b3d7a82775
misaelsoares1 <mmisasoaresms2@gmail.com>
2023-05-05 23:43:36 -0300

    Add: login + valida se usuário ja existe na hora de criar conta

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { request } from 'express';
 const app = express();
 app.use(express.json());
 const bcrypt = require('bcrypt')
@@ -10,12 +10,21 @@ const saltRounds = 1
 //USAR bcrypt
 //FAZER COMMIT
 
-let usuarios = []
-//Adiciona usuario
-app.post("/usuarios", (request, response)=>{
-    let usuario = request.body
+//cuidar regras: 
+//criar middleware para validar se existe contas com o mesmo email
 
-    bcrypt.hash(usuario.senha, saltRounds, function(err, hash) {
+let usuarios = []
+
+//Adiciona usuario
+app.post("/usuarios",(request, response)=>{
+    let usuario = request.body
+    
+    let valida = usuarios.find(user => user.email === usuario.email)
+    
+    if(valida){
+        return response.status(400).json("Usuario já existe.")
+    }else{
+        bcrypt.hash(usuario.senha, saltRounds, function(err, hash) {
         if(hash){
             usuarios.push({
                 id: Math.floor(Math.random()*67676),
@@ -25,18 +34,41 @@ app.post("/usuarios", (request, response)=>{
                 email: usuario.email,
                 senha: hash
             }); 
+            
             return response.status(204).json();
         } else {
             return response.status(400).json("Ocorreu um erro:" + err)
         }
-    });
+    })
+}
+
+
 })
 
 
 
 
 //Login
+app.post("/usuarios/login", (request, response)=>{
+    let login = request.body
+    let entrarEmail = usuarios.some(user => user.email === login.email)
+    //let entrarSenha = usuarios.find(user => user.senha === login.senha)
+    
 
+    if(entrarEmail){
+        let index = usuarios.findIndex(user => user.email == login.email)
+        
+        bcrypt.compare(login.senha, usuarios[index].senha, (err, hash)=>{
+            if(hash){
+                return response.status(200).json("login com sucesso")
+            }else{
+                return response.status(400).json("Email ou senha não existe.")
+            }
+        })
+    }
+    
+    
+})
 
 
 
